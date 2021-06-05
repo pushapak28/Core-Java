a6c47a6ea68da66d82f3ed4446ca0c32a59ffb95
Pushapak Kumar <pp281997@gmail.com>
2021-06-05 18:07:01 +0530

    Reverse array elements

---
+++ ReverseEleArray.java
@@ -0,0 +1,9 @@
+int [] arr = new int [] {1, 2, 3, 4, 5};  
+		//int[] array = {1,2,3,4,5};
+		for (int i = 0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		for (int i = arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]+" ");
+		}
