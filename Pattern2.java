450d27d6ef624eec04e8d95bf137ee3b4a923048
Pushapak Kumar <pp281997@gmail.com>
2021-06-06 22:15:26 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -14,7 +14,28 @@ public class Pattern2 {
 			}
 			System.out.println();
 		}
-	
+	//PAttern 
+		
+				for(int k =1 ;k<=n;k++) {
+					for(int j=1;j<=k-1;j++) {
+						System.out.print("  ");
+					}
+					for(int j=1;j<=n-k+1;j++) {
+						System.out.print("* ");
+					}
+					System.out.println();
+				}
+		
+		for(int i =1 ;i<=n;i++) {
+			
+			for(int j=1;j<=n-i+1;j++) {
+				System.out.print("  ");
+			}
+			for(int j=1;j<=i-1;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 		sc.close();
 	}
 }
