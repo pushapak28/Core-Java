89853fe093ca30d22489cb12f284e2756a4197b0
Pushapak Kumar <pp281997@gmail.com>
2021-06-20 09:35:40 +0530

    StringBuilderReverse

---
+++ StringBuilderReverse.java
@@ -0,0 +1,16 @@
+package string;
+
+public class StringBuilderReverse {
+
+	public static void main(String[] args) {
+		
+		String str = "pushapak kumar  b aghel";
+		
+		StringBuilder stringBuilder = new StringBuilder(str);
+		
+//		stringBuilder.reverse();
+		System.out.println(stringBuilder.reverse());
+
+	}
+
+}
