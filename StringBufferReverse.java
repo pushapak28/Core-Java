aba8e8615662e08fdddb193106c0b8f7da87c3fc
Pushapak Kumar <pp281997@gmail.com>
2021-06-20 09:35:16 +0530

    StringBufferReverse

---
+++ StringBufferReverse.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringBufferReverse {
+
+	public static void main(String[] args) {
+		
+		String str = "Pushapak Kumar Baghel";
+		
+		StringBuffer stringBuffer = new StringBuffer(str);
+		System.out.println(stringBuffer.reverse());
+	}
+
+}
